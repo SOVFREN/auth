a786cc2755b131a923b64c9ff868bcae20e6db0f
Michael <michael.curry88@outlook.com>
2025-03-17 12:58:32 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,23 @@
 <?php
+require 'vendor/autoload.php';
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
 
-echo "SSO System is working!";
-?>
+header('Content-Type: application/json');
 
-?>
+$secret_key = "your_secret_key"; // Change this to a secure key
+$users = [
+    "admin" => "password123",
+    "user" => "userpass"
+];
+
+// Read input JSON
+$data = json_decode(file_get_contents("php://input"), true);
+
+if (!isset($data['username']) || !isset($data['password'])) {
+    echo json_encode(["error" => "Missing credentials"]);
+    exit;
+}
+
+$username = $data['username'];
+$password = $
